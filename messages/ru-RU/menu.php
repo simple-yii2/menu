ab8389393e1785440d91c55a08a2820c3870cfbf
Dmitry Khlystov <dkhlystov@gmail.com>
2016-07-18 01:40:51 +1000

    Translation added

--- messages/ru-RU/menu.php
+++ messages/ru-RU/menu.php
@@ -1,4 +1,25 @@
 <?php
 
 return [
+	'Main menu' => 'Главное меню',
+	'Create menu item' => 'Добавить элемент меню',
+
+	'Changes saved successfully.' => 'Изменения успешно сохранены.',
+	'Menu item not found.' => 'Элемент меню не найден.',
+	'Menu item deleted successfully.' => 'Элемент меню успешно удален.',
+
+	'Name' => 'Наименование',
+	'Active' => 'Активный',
+	'Type' => 'Тип',
+	'Url' => 'Адрес ссылки',
+	'Resource' => 'Ресурс',
+
+	'Save' => 'Сохранить',
+	'Cancel' => 'Отмена',
+	'Create' => 'Добавить',
+
+	'Section' => 'Раздел',
+	'Link' => 'Ссылка',
+	'Page' => 'Страница',
+	'Gallery' => 'Фотогалерея',
 ];
