c163169dea382e06bf1727b2ebcef80fd0beb193
Dmitry Khlystov <dkhlystov@gmail.com>
2017-01-18 01:11:50 +1000

    Set menu item activity

--- frontend/helpers/Menu.php
+++ frontend/helpers/Menu.php
@@ -4,6 +4,7 @@ namespace cms\menu\frontend\helpers;
 
 use Yii;
 use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
 
 use cms\menu\common\models;
 
@@ -12,6 +13,11 @@ use cms\menu\common\models;
  */
 class Menu
 {
+
+	/**
+	 * @var array Path info of current request
+	 */
+	private static $_pathInfo;
 
 	/**
 	 * Get main menu items
@@ -31,7 +37,8 @@ class Menu
 		$objects = array_merge([$root], $root->children()->all());
 
 		$i = 0;
-		$item = self::makeBranch($objects, $i, $activeOnly);
+		$a = false;
+		$item = self::makeBranch($objects, $i, $a, $activeOnly);
 
 		return ArrayHelper::getValue($item, 'items', []);
 	}
@@ -40,24 +47,27 @@ class Menu
 	 * Make menu branch
 	 * @param models\Menu[] $objects 
 	 * @param integer &$i 
-	 * @param boolean $activeOnly 
+	 * @param boolean &$isActive Is true if branch is currently selected
+	 * @param boolean $activeOnly Only object with active = true
 	 * @return array
 	 */
-	private static function makeBranch($objects, &$i, $activeOnly)
+	private static function makeBranch($objects, &$i, &$isActive, $activeOnly)
 	{
 		$object = $objects[$i];
+		$url = $object->createUrl();
 
 		$result = [
 			'label' => $object->name,
-			'url' => $object->createUrl(),
+			'url' => $url,
 		];
 
+		$a = false;
 		$items = [];
 		while (($i < sizeof($objects) - 1) && $objects[$i + 1]->depth > $object->depth) {
 			$i++;
 			$o = $objects[$i];
 
-			$item = self::makeBranch($objects, $i, $activeOnly);
+			$item = self::makeBranch($objects, $i, $a, $activeOnly);
 
 			if (!$activeOnly || $o->active)
 				$items[] = $item;
@@ -65,7 +75,52 @@ class Menu
 		if (!empty($items))
 			$result['items'] = $items;
 
+		if ($a || self::isActive($url)) {
+			$result['active'] = true;
+			$isActive = true;
+		}
+
 		return $result;
+	}
+
+	/**
+	 * Function is determine that [[url]] is corresponds to current request
+	 * @param string||array $url 
+	 * @return boolean
+	 */
+	private static function isActive($url)
+	{
+		if (self::$_pathInfo === null)
+			self::$_pathInfo = self::getPathInfo(Yii::$app->getRequest()->absoluteUrl);
+
+		$pathInfo = self::$_pathInfo;
+		$urlInfo = self::getPathInfo($url);
+
+		if ($urlInfo['host'] !== null && $urlInfo['host'] !== $pathInfo['host'])
+			return false;
+
+		if ($urlInfo['path'] !== $pathInfo['path'])
+			return false;
+
+		return empty(array_diff_assoc($urlInfo['params'], $pathInfo['params']));
+	}
+
+	/**
+	 * Parsing url to path info
+	 * @param string||array $url 
+	 * @return array
+	 */
+	private static function getPathInfo($url)
+	{
+		$info = parse_url(Url::to($url));
+
+		parse_str(ArrayHelper::getValue($info, 'query', ''), $params);
+
+		return [
+			'host' => ArrayHelper::getValue($info, 'host'),
+			'path' => ArrayHelper::getValue($info, 'path'),
+			'params' => $params,
+		];
 	}
 
 }
